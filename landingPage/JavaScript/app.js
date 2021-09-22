025418a270db9010409d847ed418449e866f4ccb
Rosario Jauregui <rjjauregui@MacBook-Pro.local>
2021-09-22 09:29:57 -0700

    worked on menu

--- landingPage/JavaScript/app.js
+++ landingPage/JavaScript/app.js
@@ -1,9 +1,17 @@
 //Selectors
-
+const sideBar = document.querySelector('.sideBar');
+const menuBtn = document.querySelector('.menuButton');
 
 //Event Listeners
-
+menuBtn.addEventListener('click', openCloseSideBar);
 
 //Functions
+function openCloseSideBar(event){
+    console.log("GeeksforGeeks");
+    sideBar.style.width = "0px";
+}
 
-
+// finish up menue 
+// finish layout of all other componets/ other javascript functionality 
+// make it look pretty
+// moving it to full page dynamic
