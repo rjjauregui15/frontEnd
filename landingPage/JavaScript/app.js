1cca02874ca8af827146e33d4d5d5936c3718391
Rosario Jauregui <rjjauregui@college10-169-233-115-212.resnet.ucsc.edu>
2021-10-03 16:40:17 -0700

    working on sidebar features

--- landingPage/JavaScript/app.js
+++ landingPage/JavaScript/app.js
@@ -1,15 +1,34 @@
 //Selectors
 const sideBar = document.querySelector('.sideBar');
+const sideBarBtn = document.querySelector('.sideBarButton');
 const menuBtn = document.querySelector('.menuButton');
 
 //Event Listeners
-menuBtn.addEventListener('click', openCloseSideBar);
+sideBarBtn.addEventListener('click', CloseSideBar);
+menuBtn.addEventListener('click', OpenSideBar);
+
 
 //Functions
-function openCloseSideBar(event){
+function CloseSideBar(event){
     console.log("GeeksforGeeks");
     sideBar.style.width = "0px";
+    document.body.style.background = "rgba(0, 0, 0, 0)";
+
+    // document.querySelector('body').style.filter = "brightness(1)";
 }
+
+function OpenSideBar(event){
+    console.log("GeeksforGeeks2222");
+    sideBar.style.width = "40%";
+    document.body.style.background = "rgba(0, 0, 0, 0.5)";
+    // document.querySelector('body').style.filter = "brightness(50%)";
+    // sideBar.style.filter = "brightness(100%)";
+}
+
+// finish everything before we mess with more than 750px
+// even style
+
+
 
 // finish up menue 
 // finish layout of all other componets/ other javascript functionality 
