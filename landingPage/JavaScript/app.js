c5c245e11ec9493deaf1818651d14bdf4e4f081b
Rosario Jauregui <rjjauregui@MacBook-Pro.local>
2021-10-05 09:27:09 -0700

    finished sidebar

--- landingPage/JavaScript/app.js
+++ landingPage/JavaScript/app.js
@@ -13,14 +13,17 @@ function CloseSideBar(event){
     console.log("GeeksforGeeks");
     sideBar.style.width = "0px";
     document.body.style.background = "rgba(0, 0, 0, 0)";
-
+    document.querySelector('.dimOver').style.height = "0";
     // document.querySelector('body').style.filter = "brightness(1)";
 }
 
 function OpenSideBar(event){
     console.log("GeeksforGeeks2222");
-    sideBar.style.width = "40%";
-    document.body.style.background = "rgba(0, 0, 0, 0.5)";
+    sideBar.style.width = "50%";
+
+    document.querySelector('.dimOver').style.height = "100%";
+
+    // document.body.style.background = "rgba(0, 0, 0, 0.5)";
     // document.querySelector('body').style.filter = "brightness(50%)";
     // sideBar.style.filter = "brightness(100%)";
 }
@@ -30,7 +33,8 @@ function OpenSideBar(event){
 
 
 
-// finish up menue 
+// finish header bar -----  change the font ------
+// change staring menu position
 // finish layout of all other componets/ other javascript functionality 
 // make it look pretty
 // moving it to full page dynamic
